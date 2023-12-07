7e46db3d7928c34810f8165d839cf4cdc325fe1b
vanya992 <111580069+vanya992@users.noreply.github.com>
2023-12-07 08:16:20 +0100

    navigation hide/show function done

---
+++ js/nav.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  updateLoginLogoutLinks();
+});
+
+function updateLoginLogoutLinks() {
+  const isLoggedIn = checkLoginState();
+  const logInLink = document.getElementById("logIn");
+  const logOutLink = document.getElementById("logOut");
+  const registerLink = document.getElementById("register");
+  const createListing = document.getElementById("createListing");
+
+  if (isLoggedIn) {
+    logInLink.classList.add("d-none");
+    registerLink.classList.add("d-none");
+    createListing.classList.remove("d-none");
+    logOutLink.classList.remove("d-none");
+  } else {
+    logInLink.classList.remove("d-none");
+    registerLink.classList.remove("d-none");
+    createListing.classList.add("d-none");
+    logOutLink.classList.add("d-none");
+  }
+}
+
+function checkLoginState() {
+  const token = localStorage.getItem("token");
+  return token != null;
+}
