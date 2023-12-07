fd53120410454367faf6777fe63b37b78510a9ec
vanya992 <111580069+vanya992@users.noreply.github.com>
2023-12-07 08:17:14 +0100

    api addresses for fetching

---
+++ js/api/constants.js
@@ -0,0 +1,4 @@
+export const base_url = "https://api.noroff.dev/api/v1/auction";
+export const LOGIN = `${base_url}/auth/login`;
+export const REGISTER = `${base_url}/auth/register`;
+export const LISTINGS = "https://api.noroff.dev/api/v1/auction/listings";
